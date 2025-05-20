e47d5c5374ced9b80e21097707abb9678c8a5c26
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-05-20 23:48:30 +0200

    index4

---
+++ index4.php
@@ -0,0 +1,29 @@
+<?php 
+
+    $json = file_get_contents("./Rooms.json");
+    $roomArray = json_decode($json);
+
+
+    function get_room_by_id($room){
+        $selectedId = $_GET["id"];
+
+        return $room->room_id == $selectedId;
+    }
+
+        
+    $filtered = array_filter($roomArray , "get_room_by_id" );
+
+
+    echo "<ol>";
+
+    foreach($filtered as $filterKey => $filterValue){
+        foreach( $filterValue as $key => $value){
+
+            echo "<li>{$key}: {$value}</li>";
+        }
+    }
+    
+
+    echo "</ol>";
+  
+?>
